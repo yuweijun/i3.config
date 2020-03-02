111e24329b9b25f1953b2a6274f25a720a097d80
Weijun Yu <weijun.yu@sap.com>
2020-03-02 21:36:52 +0800

    add manjaro i3 vm config for virtual machine

---
+++ README.md
@@ -0,0 +1,5 @@
+# virtual machine config for i3 wm
+
+install i3status from gir, and then:
+
+    ./install.sh
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -sfn .Xresources ~/.Xresources
+ln -sfn config ~/.i3/config
+ln -sfn status.toml ~/.i3/status.toml
+ln -sfn i3status-rs ~/.i3/i3status-rs
