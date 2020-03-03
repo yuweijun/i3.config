7e4705ea450f4ebea9dc3f0f75f7bfcb8b9f49d6
yuweijun <yuweijun@live.com>
2020-03-03 23:08:53 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ sudo pacman-mirrors -ic China -m rank
 sudo pacman -Syy --noconfirm
 sudo pacman -S --noconfirm i3-gaps picom conky xorg-xrdb xorg-xrandr i3status-manjaro rxvt-unicode neovim chromium ttf-dejavu ttf-dejavu-sans-mono-powerline
 
-ln -sfn .profile ~/.profile
-ln -sfn .Xresources ~/.Xresources
-ln -sfn mimeapps.list ~/.config/mimeapps.list
+cp .profile ~/.profile
+cp .Xresources ~/.Xresources
+cp mimeapps.list ~/.config/mimeapps.list
 
-ln -sfn config ~/.i3/config
-ln -sfn status.toml ~/.i3/status.toml
-ln -sfn i3status-rs ~/.i3/i3status-rs
+cp config ~/.i3/config
+cp status.toml ~/.i3/status.toml
+cp i3status-rs ~/.i3/i3status-rs
