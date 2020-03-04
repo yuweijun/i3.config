8c8eeb2eeb7ff7429e5a24b1f52b15839dace5e6
yuweijun <yuweijun@live.com>
2020-03-04 12:28:05 +0800

    update config of i3

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # virtual machine config for i3 wm
 
-## preconditions
-
 $ sudo pacman -S --noconfirm i3-gaps picom conky xorg-xrdb xorg-xrandr i3status-manjaro rxvt-unicode neovim chromium ttf-dejavu ttf-dejavu-sans-mono-powerline
 
 install i3status-rust from [https://github.com/greshake/i3status-rust/](https://github.com/greshake/i3status-rust/), and then:
 
-## install config files of i3wm
+## install in vmware
 
     ./install.sh
+
+## install in virtualbox
+
+    ./install.sh
+    sed -i 's/ens33/enp0s3/' ~/.i3/status.toml
+
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ xrandr --output Virtual1 --primary --mode 1680x1050
 
 sudo pacman-mirrors -ic China -m rank
 sudo pacman -Syy --noconfirm
-sudo pacman -S --noconfirm i3-gaps picom conky xorg-xrdb xorg-xrandr i3status-manjaro rxvt-unicode neovim chromium ttf-dejavu ttf-dejavu-sans-mono-powerline zsh zsh-autosuggestions
+sudo pacman -S --noconfirm i3-gaps picom conky xorg-xrdb xorg-xrandr i3status-manjaro rxvt-unicode neovim chromium ttf-dejavu ttf-dejavu-sans-mono-powerline zsh zsh-autosuggestions zsh-syntax-highlighting
 
 cp .profile ~/.profile
 cp .Xresources ~/.Xresources
@@ -13,3 +13,4 @@ cp mimeapps.list ~/.config/mimeapps.list
 cp config ~/.i3/config
 cp status.toml ~/.i3/status.toml
 cp i3status-rs ~/.i3/i3status-rs
+
