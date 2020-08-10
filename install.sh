3a62c5ff1ea7e1e70c8d4b8aa433c1fa7e64ce3e
Weijun Yu <weijun.yu@sap.com>
2020-08-10 14:01:02 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ sudo pacman -Syy --noconfirm
 sudo pacman -S --noconfirm gnome-terminal
 sudo pacman -S --noconfirm neovim tmux chromium
 sudo pacman -S --noconfirm zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S --noconfirm xorg-xrdb xorg-xrandr
+sudo pacman -S --noconfirm wqy-microhei
+sudo pacman -S --noconfirm i3-gaps picom conky ttf-dejavu i3status-manjaro
 
-# sudo pacman -S --noconfirm xorg-xrdb xorg-xrandr
-# sudo pacman -S --noconfirm wqy-microhei
-# sudo pacman -S --noconfirm i3-gaps picom conky ttf-dejavu i3status-manjaro
-# sudo pacman -S --noconfirm open-vm-tools gtkmm gtkmm3
+sudo pacman -S --noconfirm open-vm-tools
+# sudo pacman -S --noconfirm gtkmm gtkmm3
 
 cp ~/.i3/config ~/.3/config.orig
 cp ~/.profile ~/.profile.orig
@@ -22,8 +23,8 @@ cp  ~/.config/mimeapps.list ~/.config/mimeapps.list.orig
 # cp status.min.toml ~/.i3/status.toml
 # cp i3status-rs ~/.i3/i3status-rs
 
-# sudo cp mnt.hgfs.service /etc/systemd/system/mnt.hgfs.service
-# sudo systemctl enable mnt.hgfs.service
+sudo cp mnt.hgfs.service /etc/systemd/system/mnt.hgfs.service
+sudo systemctl enable mnt.hgfs.service
 
 xrandr --output Virtual1 --primary --mode 1680x1050
 
