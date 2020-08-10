1a1b21171b03b1d5959d839f29eff289ba3fd50d
Weijun Yu <weijun.yu@sap.com>
2020-08-10 11:14:20 +0800

    install gtkmm3 for copy/paste with host

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ xrandr --output Virtual1 --primary --mode 1680x1050
 
 sudo pacman-mirrors -ic China -m rank
 sudo pacman -Syy --noconfirm
-sudo pacman -S --noconfirm i3-gaps picom conky xorg-xrdb xorg-xrandr i3status-manjaro rxvt-unicode neovim tmux chromium ttf-dejavu ttf-dejavu-sans-mono-powerline zsh zsh-autosuggestions zsh-syntax-highlighting wqy-microhei
+sudo pacman -S --noconfirm i3-gaps picom conky xorg-xrdb xorg-xrandr i3status-manjaro rxvt-unicode neovim tmux chromium ttf-dejavu ttf-dejavu-sans-mono-powerline zsh zsh-autosuggestions zsh-syntax-highlighting wqy-microhei gtkmm3
 
 cp ~/.profile ~/.profile.orig
 cp ~/.Xresources ~/.Xresources.orig
