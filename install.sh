b43e0719b253ffc34f493d15dad5ffd4b68edb56
Weijun Yu <weijun.yu@sap.com>
2020-08-10 12:24:45 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm i3-gaps picom conky xorg-xrdb xorg-xrandr i3status-manjaro rxvt-unicode neovim tmux chromium ttf-dejavu zsh zsh-autosuggestions zsh-syntax-highlighting wqy-microhei gtkmm3
+sudo pacman -S --noconfirm i3-gaps picom conky xorg-xrdb xorg-xrandr rxvt-unicode neovim tmux chromium ttf-dejavu zsh zsh-autosuggestions zsh-syntax-highlighting wqy-microhei gtkmm3
 
 cp ~/.profile ~/.profile.orig
 cp ~/.Xresources ~/.Xresources.orig
 cp  ~/.config/mimeapps.list ~/.config/mimeapps.list.orig
-
-# cp .profile ~/.profile
-# cp .Xresources ~/.Xresources
-# cp mimeapps.list ~/.config/mimeapps.list
-
-cp config ~/.i3/config
-cp status.min.toml ~/.i3/status.toml
-cp i3status-rs ~/.i3/i3status-rs
 
 sudo cp mnt.hgfs.service /etc/systemd/system/mnt.hgfs.service
 sudo systemctl enable mnt.hgfs.service
