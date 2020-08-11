0b4025a809c0386b63cd7ffc526c43c50c183345
Weijun Yu <weijun.yu@sap.com>
2020-08-11 10:37:50 +0800

    add termite and config

--- install.sh
+++ install.sh
@@ -2,20 +2,23 @@
 
 sudo pacman-mirrors -ic China -m rank
 sudo pacman -Syy --noconfirm
-sudo pacman -S --noconfirm gnome-terminal
+sudo pacman -S --noconfirm termite
 sudo pacman -S --noconfirm neovim tmux chromium
 sudo pacman -S --noconfirm zsh zsh-autosuggestions zsh-syntax-highlighting
 sudo pacman -S --noconfirm xorg-xrdb xorg-xrandr
 sudo pacman -S --noconfirm wqy-microhei
 sudo pacman -S --noconfirm i3-gaps picom conky ttf-dejavu i3status-manjaro
-sudo pacman -S --noconfirm open-vm-tools
 
+sudo pacman -S --noconfirm open-vm-tools
 # sudo pacman -S --noconfirm gtkmm gtkmm3
 
 cp ~/.i3/config ~/.i3/config.orig
 cp ~/.profile ~/.profile.orig
 cp ~/.Xresources ~/.Xresources.orig
-cp  ~/.config/mimeapps.list ~/.config/mimeapps.list.orig
+cp ~/.config/mimeapps.list ~/.config/mimeapps.list.orig
+
+mkdir -p ~/.config/termite
+cp termite.config ~/.config/termite/config
 
 cp config ~/.i3/config
 cp status.min.toml ~/.i3/status.toml
